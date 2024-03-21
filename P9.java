161249d3d9b11af6679b7e13dd19dc0f5047ca53
0111Vaibhav <vaibhavpithaya@gmail.com>
2024-03-21 22:41:09 +0530

    😊

--- P9.java
+++ P9.java
@@ -1,3 +1,29 @@
+import java.util.Scanner;
+
 public class P9 {
-    
+
+    public static int gcd(int n1, int n2) {
+
+        int min = Math.min(n1, n2);
+        while (min > 0) {
+            if (n1 % min == 0 && n2 % min == 0) {
+                break;
+            } else {
+                min--;
+            }
+        }
+        return min;
+
+    }
+
+    public static void main(String[] args) {
+        int a, b;
+        System.out.println("Enter two numbers : ");
+        Scanner as = new Scanner(System.in);
+        a = as.nextInt();
+        Scanner bs = new Scanner(System.in);
+        b = bs.nextInt();
+        System.out.println(gcd(a, b));
+
+    }
 }
